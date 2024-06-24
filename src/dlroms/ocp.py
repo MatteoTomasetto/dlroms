6d5374921ec85c5f346f734a6e4acf439b655c8c
Matteo Tomasetto <matteo.tomasetto@polimi.it>
2024-06-24 11:04:35 +0200

    ocp.py fixed

--- src/dlroms/ocp.py
+++ src/dlroms/ocp.py
@@ -20,20 +20,21 @@
 #
 # Please cite the Author if you use this code for your work/research.
 
+import numpy as np
 import torch
 from dlroms.cores import CPU, GPU
 from dlroms.dnns import Clock
 from IPython.display import clear_output
 
-def snapshots(n, sampler, core = GPU, verbose = False, folder = None, filenames = ['MU', 'Y', 'U']):
+def snapshots(n, sampler, core = GPU, verbose = False, filename = None):
     """Samples a collection of snapshots for a given OCP solver."""
-    clock = Clock()    
+    clock = Clock()
     clock.start()
     mu, y, u = [], [], []
     for seed in range(n):
         if(verbose):
             eta = "N/D" if seed == 0 else Clock.shortparse(clock.elapsed()*(n/float(seed)-1.0))
-            print("Generating snapshot n.%d... (ETA: %s)." % (seed+1, eta))
+            print("Generating snapshot n.%d... (ETA: %s)." % (seed + 1, eta))
         mu0, y0, u0 = sampler(seed)
         mu.append(mu0)
         y.append(y0)
@@ -45,10 +46,8 @@ def snapshots(n, sampler, core = GPU, verbose = False, folder = None, filenames 
     if(verbose):
         clear_output()
         print("Snapshots generated. Elapsed time: %s." % clock.elapsedTime())
-    mu, y, u = core.tensor(mu), core.tensor(y), core.tensor(u)
-    if(folder is None):
-        return mu, y, u
+    mu, y, u = np.stack(mu), np.stack(y), np.stack(u)
+    if(filename is None):
+        return core.tensor(mu, y, u)
     else:
-        torch.save(mu, folder + filenames[0].replace(".pt",""))
-        torch.save(y, folder + filenames[1].replace(".pt",""))
-        torch.save(u, folder + filenames[2].replace(".pt",""))
+        np.savez("%s.npz" % filename.replace(".npz",""), mu = mu, y = y, u = u, time = clock.elapsed())
