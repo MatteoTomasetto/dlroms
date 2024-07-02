a38fb11fff970dd27d39c77da91f2bfb8078f719
Matteo Tomasetto <matteo.tomasetto@polimi.it>
2024-07-02 15:17:22 +0200

    OCP class added in ocp.py

--- src/dlroms/ocp.py
+++ src/dlroms/ocp.py
@@ -115,3 +115,91 @@ def snapshots(n, sampler, core = GPU, verbose = False, filename = None):
         return core.tensor(mu, y, u)
     else:
         np.savez("%s.npz" % filename.replace(".npz",""), mu = mu, y = y, u = u, time = clock.elapsed())
+
+from dlroms.dnns import train # Neural networks handling
+from dlroms.roms import num2p, POD, projectup, projectdown # Proper orthogonal decomposition
+import matplotlib.pyplot as plt
+
+class OCP():
+    def __init__(self, Y, U, MU, ntrain):
+        self.Y = Y
+        self.U = U
+        self.MU = MU
+        self.ntrain = ntrain
+
+    def POD(self, which, k, decay = True, color = "black"):
+        S = self.Y if(which == "state") else self.U
+        pod, eig = POD(S[:self.ntrain], k = k)
+        
+        if decay:
+            plt.plot([i for i in range(1, k + 1)], eig, color = color, marker = 's', markersize = 5, linewidth = 1)
+            plt.ticklabel_format(axis = 'y', style = 'sci', scilimits = (0,0))
+            plt.scatter(k, eig[k-1], color = color, marker = 's', facecolors = 'none', linestyle = '--', s = 100)
+            plt.ylabel("${\sigma_{\mathrm{i}}}^{y}$", fontsize=15) if(which == "state") else plt.ylabel("${\sigma_{\mathrm{i}}}^{u}$", fontsize=15)
+            plt.title("Singular values decay");
+        
+        S_POD = projectdown(pod, S).squeeze(-1)
+        S_reconstructed = projectup(pod, S_POD)
+
+        return S_POD, S_reconstructed, pod, eig
+
+    def AE(self, which, encoder, decoder, S = None, training = True, save = True, path = 'autoencoder.pt',  *args, **kwargs):
+        if S is None:
+            S = self.Y if(which == "state") else self.U
+
+        autoencoder = encoder + decoder
+        
+        if training:
+            autoencoder.He() # NN initialization
+            train(autoencoder, S, S, self.ntrain, *args, **kwargs)
+        else:
+            autoencoder.load_state_dict(torch.load(path))
+            autoencoder.eval()
+        
+        autoencoder.freeze()
+        S_AE = encoder(S)
+        S_reconstructed = decoder(S_AE)
+
+        if save:
+            torch.save(autoencoder.state_dict(), path)
+
+        return S_AE, S_reconstructed
+
+    def PODAE(self, which, k, encoder, decoder, training = True, save = True, path = 'autoencoder.pt',  *args, **kwargs):
+        
+        S_POD, S_reconstructed_POD, pod, eig = self.POD(which, k, decay = False)
+        S_DLROM, S_reconstructed_DLROM = self.AE(which, encoder, decoder, S_POD, training, save, path,  *args, **kwargs)
+        S_reconstructed = projectup(pod, decoder(S_DLROM))
+
+        return S_DLROM, S_reconstructed, pod, eig
+
+
+    def redmap(self, phi, Qred, Ured, minmax = False, training = True, save = True, path = 'phi.pt', *args, **kwargs):
+
+        if minmax:
+            Qred_std = (Qred - Qred.min()) / (Qred.max() - Qred.min())
+            Ured_std = (Ured - Ured.min()) / (Ured.max() - Ured.min())
+            QUred = torch.cat((Qred_std, Ured_std), 1)
+        else:
+            QUred = torch.cat((Qred, Ured), 1)
+        
+        if training:
+            phi.He() # NN initialization
+            train(phi, self.MU, QUred, self.ntrain, *args, **kwargs)
+        else:
+            phi.load_state_dict(torch.load(path))
+            phi.eval()
+        
+        phi.freeze()
+        QUred_hat = phi(self.MU)
+        if minmax:
+            Qred_hat = QUred_hat[:,:Qred.shape[1]] * (Qred.max() - Qred.min()) + Qred.min()
+            Ured_hat = QUred_hat[:,Qred.shape[1]:] * (Ured.max() - Ured.min()) + Ured.min()
+        else:
+            Qred_hat = QUred_hat[:,:Qred.shape[1]]
+            Ured_hat = QUred_hat[:,Qred.shape[1]:]
+
+        if save:
+            torch.save(phi.state_dict(), path)
+
+        return Qred_hat, Ured_hat
